de02dbacc43ba6512ce1f38babf187f28a50831a
AlicePeach <alicepeachmelocoton2@gmail.com>
2018-05-06 12:45:15 -0500

    productos

---
+++ Proyeto-Web-gh-pages/js/add.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+ 	$('.slider').slider();
+ 	$('.carousel').carousel();
+    $('.parallax').parallax();
+ });
+
+function Ejecuta() {
+ 	alert("Mensaje enviado, Tus comentarios nos ayudan a seguir creciendo")
+ }
